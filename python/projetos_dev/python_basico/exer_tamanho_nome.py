1af08cca4cf49ab7d833c0111abfaddaff8597a3
jonfisik <development.wj@outlook.com>
2020-11-08 11:19:25 -0300

    Update exer_tamanho_nome.py

--- python/projetos_dev/python_basico/exer_tamanho_nome.py
+++ python/projetos_dev/python_basico/exer_tamanho_nome.py
@@ -11,20 +11,22 @@ traco()
 print('TAMANHO NOME v1.0'.center(50))
 traco()
 
-try:
-    nome = str(input('Digite seu primeiro nome: ')).capitalize().strip()
-    if nome.isnumeric():
-        print('Teu nome é número?')
-    elif not int(nome) == nome:
-        print('Escreve esse nome direito.')
-except:
-    if nome == 'Jonatan':
-        print('Esse nome é lindo!!!')
-    elif 5 <= len(nome) <= 6:
-        print(f'{nome}, seu nome é normalzinho. Dá pra o gasto.')
-    elif len(nome) <= 4:
-        print(f'{nome}, que nome curto. Teu pai é economista?')
-    elif len(nome) > 6:
-        print(f'Nossa {nome}, que exageiro de nome!')
+
+nome = str(input('Digite seu primeiro nome: ')).capitalize().strip()
+# if nome.isnumeric():
+#     print('Teu nome é número?')
+#     traco()
+# elif not nome.isnumeric():
+#     print('Escreve esse nome direito.')
+#exit()
+
+if nome == 'Jonatan':
+    print('Esse nome é lindo!!!')
+elif 5 <= len(nome) <= 6:
+    print(f'{nome}, seu nome é normalzinho. Dá pra o gasto.')
+elif len(nome) <= 4:
+    print(f'{nome}, que nome curto. Teu pai é economista?')
+elif len(nome) > 6:
+    print(f'Nossa {nome}, que exageiro de nome!')
 
 traco()
