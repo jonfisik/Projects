6a33023acd4f50db354b20646f5f728603c1b01f
jonfisik <development.wj@outlook.com>
2020-11-08 12:29:58 -0300

    Update exer_tamanho_nome.py

--- python/projetos_dev/python_basico/exer_tamanho_nome.py
+++ python/projetos_dev/python_basico/exer_tamanho_nome.py
@@ -3,6 +3,7 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 se tiver 5 ou 6 letras "seu nome é normal": maior que 6 "seu nome é muito grande".
 '''
 
+
 def traco():
     print('-'*50)
 
