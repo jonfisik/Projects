e22460e3df16473400d870de26eec0dbc1397745
jonfisik <development.wj@outlook.com>
2020-11-13 15:58:52 -0300

    Update condicional_or.py

--- python/projetos_dev/python_basico/condicional_or.py
+++ python/projetos_dev/python_basico/condicional_or.py
@@ -1,3 +1,11 @@
 '''
-
+Operador or
 '''
+nome = input('Qual seu nome? ')
+if nome:
+    print(nome)
+else:
+    print('Você não digitou nada =(')
+#--------------------------------------
+nome = input('Qual seu nome? ')
+print(nome or 'Você não digitou nada!')
