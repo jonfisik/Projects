75a59e7c98980d19ff0c4dcf8785b0f6dbf4be72
jonfisik <development.wj@outlook.com>
2020-11-15 07:50:18 -0300

    Create file

---
+++ python/projetos_dev/python_programacao_procedural/aula1_funcoes.py
@@ -0,0 +1,29 @@
+'''
+Funções - def em Python (parte 1)
+'''
+
+
+def funcao():
+    print('Hello World!')
+
+
+def mensagemParamentro(msg):
+    print(msg)
+
+
+def funcaoDoisParametros(msg, nome):
+    print(msg, nome)
+
+
+def funcaoValoresPadrao(msg = 'xxxx', nome = 'xxxxxxx'):
+    print(msg, nome)
+
+
+funcao()
+mensagemParamentro('Parâmetro da função')
+mensagemParamentro('Outro parâmetro')
+funcaoDoisParametros('Olá,', 'Jonatan')
+funcaoDoisParametros('Bom dia,', 'Paschoal')
+funcaoValoresPadrao()
+funcaoValoresPadrao('Boa tarde,', 'Ciclano')
+funcaoValoresPadrao(nome = 'Zé', msg = 'Olá,')
