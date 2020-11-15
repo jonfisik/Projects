b9c528aed220c9a9c6c4b01b119735e377bab2bf
jonfisik <development.wj@outlook.com>
2020-11-15 08:34:55 -0300

    Create aula2_funcoes_return.py

---
+++ python/projetos_dev/python_programacao_procedural/aula2_funcoes_return.py
@@ -0,0 +1,48 @@
+'''
+Funções (def) em Python - return - Parte 2
+'''
+
+def funcao(var):
+    #print(var)
+    return var
+    # A partir daqui nada será executado dentro dessa função.
+
+
+def divisao(n1, n2):
+    #if n2 > 0:
+        #return n1 / n2
+    if n2 == 0:
+        return
+    return n1 / n2
+
+
+variavel = funcao('Valor que eu quero.')
+
+if variavel:
+    print(variavel)
+else:
+    print('Nenhum Valor.')
+
+divide = divisao(8,10)
+
+if divide:
+    print(divide)
+else:
+    print('Conta inválida.')
+
+print('-'*20)
+def f(var):
+    print(var)
+
+
+def dumb():
+    return f
+
+
+var = dumb()
+print(id(var), id(f))
+
+if var == f:
+    print('var é igual a f.')
+else:
+    print('Blaaaaaaa')
