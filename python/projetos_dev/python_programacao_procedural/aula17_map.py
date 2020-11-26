5bd27f2eb5b5d1cdc9300a699f19e01af2b0c752
jonfisik <development.wj@outlook.com>
2020-11-26 07:29:13 -0300

    Create aula17_map.py

---
+++ python/projetos_dev/python_programacao_procedural/aula17_map.py
@@ -0,0 +1,29 @@
+'''
+map
+'''
+produto = [
+    {'nome': 'p1', 'preco': 50},
+    {'nome': 'p2', 'preco': 10.9},
+    {'nome': 'p3', 'preco': 20},
+    {'nome': 'p4', 'preco': 30},
+    {'nome': 'p5', 'preco': 40},
+    {'nome': 'p6', 'preco': 10},
+    {'nome': 'p7', 'preco': 8.7},
+    {'nome': 'p8', 'preco': 3.9},
+    {'nome': 'p9', 'preco': 23.5},
+    {'nome': 'p10', 'preco': 50.5},
+]
+
+pessoas = [
+    {'nome': 'João', 'idade': 18},
+    {'nome': 'Pedro', 'idade': 32},
+    {'nome': 'Tiago', 'idade': 35},
+    {'nome': 'Felipe', 'idade': 42},
+    {'nome': 'Mateus', 'idade': 50},
+    {'nome': 'Bartolomeu', 'idade': 43},
+    {'nome': 'André', 'idade': 55},
+    {'nome': 'Tómas', 'idade': 23},
+    {'nome': 'JUdas', 'idade': 39},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
