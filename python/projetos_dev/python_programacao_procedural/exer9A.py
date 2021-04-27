27620f8a4b115805cc7ec5e8ee133139d6845f33
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-04-27 20:05:15 -0300

    Update exer9A.py

--- python/projetos_dev/python_programacao_procedural/exer9A.py
+++ python/projetos_dev/python_programacao_procedural/exer9A.py
@@ -42,42 +42,42 @@ while True:
             except:
                 print('Digite apenas números.')
                 pass
-        if acao > 5:
-            print('O numero digitado não corresponde a nenhuma opção.')
-        elif acao < 1:
-            print('O número digitado não corresponde a nenhuma opção.')
-        else:
+            if acao > 5:
+                print('O numero digitado não corresponde a nenhuma opção.')
+            elif acao < 1:
+                print('O número digitado não corresponde a nenhuma opção.')
+            else:
+                break
+        if acao == 1:
+            adicionarTarefa()
+        elif acao == 2:
+            print(f'\033[m{" "}', end='')
+            print(f'\033[1m-='*20)
+            print(f'\033[1;32 {"<<<LISTA>>>": ^40}')
+            mostrarLista(lista)
+            print(f'\033[m{" "}', end='')
+            print('\033[1m-='*20)
+        elif acao == 3:
+            try:
+                desfazer(lista)
+            except:
+                print('\033[1;31mNão há itens para desfazer'
+                    '\nPor favor digite novamente!\033[m')
+                pass
+        elif acao == 4:
+            try:
+                lista.append(temporario[len(temporario)-1])
+                temporario.pop()
+            except:
+                print('\033[1;31mNão há itens para refazer'
+                    '\nPor favor digite novamente!\033[m')
+                pass
+        elif acao == 5:
+            nome = input('Digite o nome do arquivo do arquivo para gerar sua lista (max 5 caracteres): ')[:6]
+            nome += '.txt'
+            titulo = input('Digite um título para sua lista: ').upper
             break
-    if acao == 1:
-        adicionarTarefa()
-    elif acao == 2:
-        print(f'\033[m{" "}', end='')
-        print(f'\033[1m-='*20)
-        print(f'\033[1;32 {"<<<LISTA>>>": ^40}')
-        mostrarLista(lista)
-        print(f'\033[m{" "}', end='')
-        print('\033[1m-='*20)
-    elif acao == 3:
-        try:
-            desfazer(lista)
-        except:
-            print('\033[1;31mNão há itens para desfazer'
-                  '\nPor favor digite novamente!\033[m')
-            pass
-    elif acao == 4:
-        try:
-            lista.append(temporario[len(temporario)-1])
-            temporario.pop()
-        except:
-            print('\033[1;31mNão há itens para refazer'
-                  '\nPor favor digite novamente!\033[m')
-            pass
-    elif acao == 5:
-        nome = input('Digite o nome do arquivo do arquivo para gerar sua lista (max 5 caracteres): ')[:6]
-        nome += '.txt'
-        titulo = input('Digite um título para sua lista: ').upper
-        break
-    
+    break  
 with open(nome, 'a+') as file:
     file.write(f'{titulo}\n')
     for itens in lista:
