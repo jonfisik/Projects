7120f0bfaab476a71be4be1c967564cafef7eb57
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-06-01 21:33:53 -0300

    Create files

---
+++ python/AlgoritmosPython/Maximo.py
@@ -0,0 +1,27 @@
+__author__  = 'JPaschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__date__    = '01/06/2021'
+"""
+Dados dois naturais m e n determinar, entre todos os pares de números naturais
+(x,y) tais que x < m e y < n, um par para o qual o valor da expressão
+x*y - x**2 + y seja máximo e calcular também esse máximo.
+"""
+#-----------------------------------------------------------
+def traco():
+    return print('-----'*10)
+
+print('')
+print("Naturais - ")
+traco()
+
+#input
+
+#-----------------------------------------------------------
+
+
+
+print('')
+traco()
+print('')
+# END
---
+++ python/AlgoritmosPython/imparesConsecutivos.py
@@ -0,0 +1,29 @@
+__author__  = 'JPaschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__date__    = '01/06/2021'
+"""
+Sabe-se que um número da forma n**3 é igual a soma de n ímpares consecutivos.
+
+Exemplo: 1**3= 1, 2**3= 3+5, 3**3= 7+9+11,  4**3= 13+15+17+19,...
+Dado m, determine os ímpares consecutivos cuja soma é igual a n**3 para n
+assumindo valores de 1 a m. 
+"""
+#-----------------------------------------------------------
+def traco():
+    return print('-----'*10)
+
+print('')
+print("Naturais - ")
+traco()
+
+#input
+
+#-----------------------------------------------------------
+
+
+
+print('')
+traco()
+print('')
+# END
