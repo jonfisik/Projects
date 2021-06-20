9519acaa758f12f0aad6a48ab2203e109a38a6eb
jonfisik <development.wj@outlook.com>
2021-06-20 13:11:23 -0300

    Create tela.py

---
+++ python/projetos_dev/pysimplegui/tela.py
@@ -0,0 +1,32 @@
+__author__  = 'Jonatan Paschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__status__  = 'Professor - Fisica'
+__date__    = '20/06/2021'
+
+import PySimpleGUI as sg
+
+# Classe
+class TelaPython:
+    def __init__(self):
+        # Layout
+        layout =[
+            # sg.Text - cria a label, sg.Imput - recebe os dados
+            [sg.Text('Nome'),sg.Input()],
+            [sg.Text('Idade'),sg.Input()],
+            # sg.Button - cria o botão
+            [sg.Button('Enviar Dados')]
+        ]
+        # Janela
+        janela = sg.Window('Dados do usuário').layout(layout)
+        # Extrair dados da tela
+        self.button, self.values = janela.Read()
+
+    def Iniciar(self):
+        print(self.values)
+
+# Instanciando classe
+tela = TelaPython()
+tela.Iniciar()
+
+
