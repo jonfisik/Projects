58fb5d4fbfb7b533b5714abd17266be896ad2981
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-06-22 19:36:30 -0300

    Create funcaoDef.py

---
+++ python/projetos_dev/python_programacao_procedural/funcaoDef.py
@@ -0,0 +1,35 @@
+__author__  = 'Jonatan Paschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__status__  = 'Professor - Física'
+__date__    = '22/06/2021'
+'''
+Definição de funções em python.
+'''
+
+n1 = 15
+n2 = 7
+
+def traco():
+    print('--'*20)
+
+def somar():
+    res = n1 + n2
+    print('Soma de ' + str(n1) + ' e ' + str(n2) + ' = ' + str(res))
+
+def subtrair():
+    res = n1 - n2
+    print('A diferença de ' + str(n1) + ' e ' + str(n2) + ' = ' + str(res))
+
+def multiplicar():
+    res = n1 * n2
+    print('O produto de ' + str(n1) + ' e ' + str(n2) + ' = ' + str(res))
+
+
+print('')
+print('Definição Função')
+traco()
+somar()
+subtrair()
+multiplicar()
+traco()
