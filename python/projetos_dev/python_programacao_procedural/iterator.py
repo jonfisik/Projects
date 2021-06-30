268afd9ac08458151a7a900f20b8d7c89386bbab
jonfisik <development.wj@outlook.com>
2021-06-30 08:52:16 -0300

    Create iterator.py

---
+++ python/projetos_dev/python_programacao_procedural/iterator.py
@@ -0,0 +1,19 @@
+__author__  = 'Jonatan Paschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__status__  = 'Professor - Física'
+__date__    = '30/06/2021'
+'''
+Iterator
+'''
+
+carros = ["HRV", "Polo", "Jetta", "Fusion","Honda Fit"]
+
+itCarros = iter(carros)
+
+print(next(carros))
+print(next(carros))
+print(next(carros))
+print(next(carros))
+print(next(carros))
+print(next(carros))
