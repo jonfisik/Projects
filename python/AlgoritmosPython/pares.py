f60963b461ceebd049faf26e3bf1b0ef4500bd27
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-09-13 12:56:28 -0300

    Update pares.py

--- python/AlgoritmosPython/pares.py
+++ python/AlgoritmosPython/pares.py
@@ -3,14 +3,9 @@ __version__ = '1.0.0'
 __email__   = 'jonfisik@hotmail.com'
 __date__    = '08/06/2021'
 """
-"""
-Dados três números naturais,
-verificar se eles formam os lados de um triângulo retângulo.
-
-lado1**2 + lado2**2 == lado3**2
-lado3**2 + lado1**2 == lado2**2
-lado2**2 + lado3**2 == lado1**2
-"""
+Dados dois naturais m e n determinar, entre todos os pares de números naturais
+(x,y) tais que x < m e y < n, um par para o qual o valor da expressão
+x*y - x**2 + y seja máximo e calcular também esse máximo.
 """
 #-----------------------------------------------------------
 def traco():
@@ -37,9 +32,6 @@ for x in range(m):
 print(f'O maior par é ({Xmax}, {Ymax}) e seu valor máximo é {Vmax}.')
 
 #-----------------------------------------------------------
-
-
-
 print('')
 traco()
 print('')
