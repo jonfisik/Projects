d28c9de806e3efc0eaead152d12afd383518cd13
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-09-13 11:51:36 -0300

    Create jogoVelha.py

---
+++ python/projetos_dev/python_programacao_procedural/jogoVelha.py
@@ -0,0 +1,8 @@
+__author__  = 'Jonatan Paschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__status__  = 'Professor - Física'
+__date__    = '29/06/2021'
+'''
+Jogo da velha 
+'''
