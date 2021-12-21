9c24b0cb1ea68a7879eaf009a9a3a341db28dce8
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-12-21 19:29:35 -0300

    Update jogoVelha.py

--- python/projetos_dev/class_python/jogoVelha.py
+++ python/projetos_dev/class_python/jogoVelha.py
@@ -16,7 +16,7 @@ from colorama import Fore, Back, Style
 #
 jogarNovamente = 's'
 jogadas = 0
-quemJogar = 2
+quemJoga = 2 # 1 = CPU - 2 = Jogador
 maxJogadas = 9
 vit = 'n' #Vitória
 velha = [
@@ -37,9 +37,40 @@ def tela():
     print('2:  ' + velha[2][0] + ' | ' + velha[2][1] + ' | ' + velha[2][2])
     print('Jogadas: ' + Fore.GREEN + str(jogadas) + Fore.RESET)
 
+def jogadorJoga():
+    global jogadas
+    global quemJoga
+    global maxJogadas
+    if quemJoga == 2 and jogadas < maxJogadas:
+        try:
+            l = int(input('Linha..: ')) 
+            c = int(input('Coluna.: '))
+            while velha[l][c] != ' ':
+                l = int(input('Linha..: ')) 
+                c = int(input('Coluna.: '))
+            velha[l][c] = 'X'
+            quemJoga = 1
+            jogadas += 1
+        except:
+            print('')
+            print('JOGADA INVÁLIDA!')
+            os.system('pause')
+ 
+def cpuJoga():
+    global jogadas
+    global quemJoga
+    global maxJogadas
+    if quemJoga == 1 and jogadas < maxJogadas:
+        l = random.randrange(0,3)
+        c = random.randrange(0,3)
+        while velha[l][c] != ' ':
+            l = random.randrange(0,3)
+            c = random.randrange(0,3)
+        velha[l][c] = 'O'
+        jogadas += 1
+        quemJoga = 2
+
 while True:
     tela()
-    #jogadaJogador
-    #jogadaCPU
-    #verificarVitoria
-    break
+    jogadorJoga()
+    cpuJoga()
