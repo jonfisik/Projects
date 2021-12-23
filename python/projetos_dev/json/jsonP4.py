de8e144b425b64504af3ca0b18c8047a0585c7d4
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2021-12-23 11:19:04 -0300

    Create jsonP4.py

---
+++ python/projetos_dev/json/jsonP4.py
@@ -0,0 +1,22 @@
+__author__  = 'Jonatan Paschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__status__  = 'Professor - Física & Matemática'
+__date__    = '23/12/2021'
+'''
+Manipulando json
+'''
+{
+    'nome':'Bruno',
+    'time':'aviadores',
+    'vivo':'True',
+    'energia':'1000',
+    'mochila':['pederneira','corda','linha','arame'],
+    'aeronaves':[
+        {'tipo':'transporte','habilidade':800},
+        {'tipo':'ataque','habilidade':1000},
+        {'tipo':'reconhecimento','habilidade':50}
+    ]
+}
+
+jogador_j = '{"nome":"Bruno","time":"aviadores","vivo":"True","energia":"1000","mochila":["pederneira","corda","linha","arame"],"aeronaves":[{"tipo":"transporte","habilidade":800},{"tipo":"ataque","habilidade":1000},{"tipo":"reconhecimento","habilidade":50}]}'
