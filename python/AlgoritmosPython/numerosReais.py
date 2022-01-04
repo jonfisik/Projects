1c649b25444bda9ef67b3eb98f3cac0b8de7639b
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2022-01-04 12:03:04 -0300

    Create numerosReais.py

---
+++ python/AlgoritmosPython/numerosReais.py
@@ -0,0 +1,36 @@
+__author__  = 'Jonatan Paschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__status__  = 'Professor - Física & Matemática'
+__date__    = '27/12/2021'
+'''
+#ignorância zero
+Números reais
+Dado um número inteiro positivo n, calcular e imprimir o valor da seguinte soma.
+
+S = 1/n + 2/(n-1) + 3/(n-2)+ ... + n/1
+'''
+
+#
+## Função e título
+# 
+def traco():
+    return print('----'*7)
+
+traco()
+print('Números reais - ')
+traco()
+
+#
+## input e declaração
+# 
+n = int(input('Digite um número inteiro: '))
+
+soma = 0.0
+for i in range(1, n+1):
+    soma += i/(n-i+1)
+    print(f'-> {soma}')
+
+traco()
+print(f'Soma total: {soma}.')
+traco()
