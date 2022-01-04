bf0999aee0100ef518498dc824785d8a5a4f0461
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2022-01-04 08:02:28 -0300

    Update divisoresNM.py

--- python/AlgoritmosPython/divisoresNM.py
+++ python/AlgoritmosPython/divisoresNM.py
@@ -4,22 +4,23 @@ __email__   = 'jonfisik@hotmail.com'
 __date__    = '04/01/2022'
 """
 #ignorância zero
-Dado n e dois números inteiros positivos i e j diferentes de 0, imprimir em ordem crescente os n primeiros naturais que são múltilpos de i ou de j e ou ambos.
+Dados dois números inteiros positivos i e j diferentes de 0,
+imprimir todos os divisores comuns de i e j.
 
-Exemplo: Para n = 6, i = 2 e j = 3 a saída deverá ser: 0, 2, 3, 4, 6, 8.
+Exemplo: i = 2 e j = 3 a saída deverá ser : 1
+i = 9, j = 21 a saída deverá ser: 1, 3
 """
 
 def traco():
     return print('-----'*5)
 
 print('')
-print("Multiplos até N - ")
+print("Divisores de i e j - ")
 traco()
 
 #
 ## input e variáveis
 # 
-n = int(input('Digite N: '))
 i = int(input('Digite o número i: '))
 j = int(input('Digite o número j: '))
 
@@ -27,10 +28,10 @@ nat, cont = 0, 0
 #
 ## teste lógico
 #
-while cont < n:
-    if nat % i == 0 or nat % j == 0:
+while cont < i or cont < j:
+    if i % cont == 0 and j % cont == 0:
         print('')
-        print(f'-> {nat}')
+        print(f'{i%cont} {j%cont}')
         cont = cont + 1
     nat = nat + 1
 print('')
