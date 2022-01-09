47ca3748445fad3ee29aaf1210750d63011abd33
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2022-01-09 17:03:30 -0300

    Create mediaAlunos.py

---
+++ python/AlgoritmosPython/mediaAlunos.py
@@ -0,0 +1,36 @@
+__author__  = 'Jonatan Paschoal'
+__version__ = '1.0.0'
+__email__   = 'jonfisik@hotmail.com'
+__status__  = 'Professor - Física & Matemática'
+__date__    = '09/01/2022'
+'''
+"""
+Faça um programa que calcule o número médio de alunos por turma.
+Para isto, peça a quantidade de turmas e a quantidade de alunos para cada turma.
+As turmas não podem ter mais de 40 alunos.
+'''
+#
+## Funções
+#
+
+
+
+def traco():
+    return print('----'*10)
+
+#
+## Título
+#
+traco()
+print('Media de alunos - ')
+traco()
+
+#
+## Rotina principal
+#
+
+
+
+print('FIM')
+traco()
+#END
