701fa6f0b201f3b61016ad1fd417c428c6c9dc69
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2022-01-11 11:10:27 -0300

    Update mediaAlunos.py

--- python/AlgoritmosPython/mediaAlunos.py
+++ python/AlgoritmosPython/mediaAlunos.py
@@ -5,6 +5,7 @@ __status__  = 'Professor - Física & Matemática'
 __date__    = '09/01/2022'
 '''
 """
+#ignorância zero
 Faça um programa que calcule o número médio de alunos por turma.
 Para isto, peça a quantidade de turmas e a quantidade de alunos para cada turma.
 As turmas não podem ter mais de 40 alunos.
